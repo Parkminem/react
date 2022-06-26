4e2d0a16851e7f0e958a2bb14ae6b34934aee77a
MinhyukPark <parkminem@gmail.com>
2022-06-27 00:59:15 +0900

    performance improvement

--- card-maker/src/components/card/card.jsx
+++ card-maker/src/components/card/card.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './card.module.css';
 
 const DEFAULT_IMAGE = '/images/default_logo.png';
-const Card = ({ card }) => {
+const Card = memo(({ card }) => {
   const { name, company, title, email, message, theme, fileURL } = card;
   const url = fileURL || DEFAULT_IMAGE;
   return (
@@ -17,18 +17,18 @@ const Card = ({ card }) => {
       </div>
     </li>
   );
-  function getStyles(theme) {
-    switch (theme) {
-      case 'Dark':
-        return styles.dark;
-      case 'Light':
-        return styles.light;
-      case 'Colorful':
-        return styles.colorful;
-      default:
-        throw new Error(`unknow theme: ${theme}`);
-    }
+});
+function getStyles(theme) {
+  switch (theme) {
+    case 'Dark':
+      return styles.dark;
+    case 'Light':
+      return styles.light;
+    case 'Colorful':
+      return styles.colorful;
+    default:
+      throw new Error(`unknow theme: ${theme}`);
   }
-};
+}
 
 export default Card;
